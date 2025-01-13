07cf571531a63c3094bf6314c4fcb212835631c5
Jtmixon5 <jtmixon10@gmail.com>
2025-01-12 23:45:28 -0500

    added linkedin and email

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,19 @@ function App() {
     <>
       <div className="name-section">
         <h1>Jacob Mixon</h1>
-        <h2>Software Developer</h2> 
+        <h2 className="contact-info">
+        <p>
+      <a 
+        href="https://www.linkedin.com/in/jacobmixon/"
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="linkedin-link"
+      >
+        LinkedIn
+      </a>
+       {" "}| jtmixon10@gmail.com | U.S. Citizen
+        </p>
+      </h2>
       </div>
       <div className="education-section">
         <h1>Education</h1>
